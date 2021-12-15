fd3336a1cd392ebb9971ed3ae329f4a938320f74
Al Green <a.l.green@hotmail.com>
2021-12-14 19:54:26 -0600

    update index, auth  with auth route

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,6 +1,33 @@
 const Joi = require("joi");
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcrypt");
+const { User } = require("../models/user");
+const config = require("config");
+const jwt = require("jwtwebtoken");
+
+router.post("/", async (req, res) => {
+  try {
+    const { error } = validateLogin(req.body);
+
+    if (error) return res.status(400).send(error.details[0].message);
+    let user = await User.findOne({ email: req.body.email });
+    if (!user) return res.status(400).send("Invalid email or password.");
+    
+    const validPassword = await bcrypt.compare(
+      req.body.password,
+      user.password
+    );
+    if (!validPassword)
+      return res.status(400).send("Invalid email or password.");
+
+     const token = jwt.sign({_id: user._id, name: user.name}, config.get('jwtSecret')); 
+    
+    return res.send(true);
+  } catch (ex) {
+    return res.status(500).send(`Internal Server Error: ${ex}`);
+  }
+});
 
 function validateLogin(req) {
   const schema = Joi.object({
