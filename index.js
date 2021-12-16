0bac15d1f156f32493849929fdd4c2e8bc29b18c
Al Green <a.l.green@hotmail.com>
2021-12-15 19:35:18 -0600

    update index with upload image method

--- index.js
+++ index.js
@@ -4,10 +4,12 @@ const app = express();
 const users = require('./routes/users');
 const familyCards = require('./routes/familyCards');
 const cors = require('cors');
-// const posts = require('./routes/posts');
+const auth = require('./routes/auth');
 // const cors = require('cors');
-// const fs = require('fs');
-// const path = require('path');
+const fs = require('fs');
+const path = require('path');
+const auth = require('./middleware/auth');
+const res = require('express/lib/response');
 
 connectDB();
 
@@ -15,9 +17,21 @@ app.use(cors());
 app.use(express.json());
 app.use('/api/users', users);
 app.use('/api/familyCards', familyCards);
-app.use('/api/auth', users);
+app.use('/api/auth', auth);
 
-// app.use("/uploads/images", express.static(path.join("uploads", "images")));
+app.use("/uploads/images", express.static(path.join("uploads", "images")));
+app.use((error, req, res, next) => {
+    if (req.file) {
+        fs.unlink(req.file.path, (err) => {
+            console.log(err);
+        })
+    }
+    if (res.headerSent){
+        return next(error);
+    }
+    res.status(err.code || 500);
+    res.json({message: error.message || "an unknown error occured."})
+});
 
 const port = process.env.PORT || 5020
 app.listen(port, () => {
